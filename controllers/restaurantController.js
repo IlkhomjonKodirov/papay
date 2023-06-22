4898c3cad7a21ce1dbb58c502afeff24ade73ccb
IlkhomjonKodirov <ilhom.qodirov9393@gmail.com>
2023-06-22 08:22:22 -0700

    feat: develop admin related restaurant control page

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -16,11 +16,10 @@ restaurantController.home = (req, res) => {
 };
 
 restaurantController.getMyRestaurantProducts = async (req, res) => {
-  
   try {
     console.log("GET: cont/getMyRestaurantProducts");
     const product = new Product();
-    const data = await product.getAllProductsDataResto(res.locals.member); 
+    const data = await product.getAllProductsDataResto(res.locals.member);
     res.render("restaurant-menu", { restaurant_data: data });
   } catch (err) {
     console.log(`ERROR, cont/getMyRestaurantProducts, ${err.message}`);
@@ -31,7 +30,7 @@ restaurantController.getMyRestaurantProducts = async (req, res) => {
 restaurantController.getSignupMyRestaurant = async (req, res) => {
   try {
     console.log("GET: cont/getSignupMyRestaurant");
-    res.render("signup"); 
+    res.render("signup");
   } catch (err) {
     console.log(`ERROR, cont/getSignupMyRestaurant, ${err.message}`);
     res.json({ state: "fail", message: err.message });
@@ -44,16 +43,16 @@ restaurantController.signupProcess = async (req, res) => {
     assert(req.file, Definer.general_err3);
 
     let new_member = req.body;
-    new_member.mb_type = 'RESTAURANT';
+    new_member.mb_type = "RESTAURANT";
     new_member.mb_image = req.file.path;
 
     const member = new Member();
-      // new_member = await member.signupData(data);
+    // new_member = await member.signupData(data);
     const result = await member.signupData(new_member);
     assert(result, Definer.general_err1);
 
-    req.session.member = result; 
-    res.redirect("/resto/products/menu"); 
+    req.session.member = result;
+    res.redirect("/resto/products/menu");
   } catch (err) {
     console.log(`ERROR, cont/signupProcess, ${err.message}`);
     res.json({ state: "fail", message: err.message });
@@ -63,7 +62,7 @@ restaurantController.signupProcess = async (req, res) => {
 restaurantController.getLoginMyRestaurant = async (req, res) => {
   try {
     console.log("GET: cont/getLoginMyRestaurant");
-    res.render("login-page"); 
+    res.render("login-page");
   } catch (err) {
     console.log(`ERROR, cont/getLoginMyRestaurant, ${err.message}`);
     res.json({ state: "fail", message: err.message });
@@ -92,10 +91,11 @@ restaurantController.loginProcess = async (req, res) => {
 restaurantController.logout = (req, res) => {
   try {
     console.log("GET cont/logout");
-  req.session.destroy(function(){ // sessionni destroy qiladi yani delete qiladi
-    res.redirect("/resto");
-  });
-  } catch(err) {
+    req.session.destroy(function () {
+      // sessionni destroy qiladi yani delete qiladi
+      res.redirect("/resto");
+    });
+  } catch (err) {
     console.log(`ERROR, cont/logout, ${err.message}`);
     res.json({ state: "fail", message: err.message });
   }
@@ -103,9 +103,8 @@ restaurantController.logout = (req, res) => {
 
 restaurantController.validateAuthRestaurant = (req, res, next) => {
   if (req.session?.member?.mb_type === "RESTAURANT") {
-    req.member = req.session.member; 
-    next(); 
-    console.log(req.member);
+    req.member = req.session.member;
+    next();
   } else
     res.json({
       state: "fail",
@@ -118,5 +117,31 @@ restaurantController.checkSessions = (req, res) => {
     res.json({ state: "succeed", data: req.session.member });
   } else {
     res.json({ state: "fail", message: "You are not authentication" });
+  }
+};
+
+restaurantController.validateAdmin = (req, res, next) => {
+  if (req.session?.member?.mb_type === "ADMIN") {
+    req.member = req.session.member;
+    next();
+  } else {
+    const html = `<script>
+                    alert('Admin page: Permission denied!');
+                    window.location.replace('/resto');
+                  </script>`;
+    res.end(html);
+  }
+};
+
+restaurantController.getAllRestaurants = (req, res) => {
+  try {
+    console.log("GET cont/getAllRestaurant");
+
+    //todo: hamma restaurantlarni dbdan chaqiramiz
+
+    res.render("all-restaurants");
+  } catch (err) {
+    console.log(`ERROR, cont/getAllRestaurants, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
   }
 };
