13ac9a15513bfc1572670c108b0ee94968c6f9e1
IlkhomjonKodirov <ilhom.qodirov9393@gmail.com>
2023-07-14 12:28:05 -0700

    fix: checkMyAuthentication and retrieve token object data

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -11,12 +11,12 @@ memberController.signup = async (req, res) => {
       member = new Member(), // serviceModel(Member classi)ning instancesi
       new_member = await member.signupData(data); // signupData ichiga req.bodyni yuboramiz. req.body esa Member.jsdagi inputni o'rniga boradi
 
-      const token = memberController.createToken(new_member); //
-  
-      res.cookie("access_token", token, {
-        maxAge: 6 * 3600 * 1000,
-        httpOnly: true,
-      });
+    const token = memberController.createToken(new_member); //
+
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
 
     res.json({ state: "succeed", data: new_member });
   } catch (err) {
@@ -47,10 +47,11 @@ memberController.login = async (req, res) => {
 };
 
 memberController.logout = (req, res) => {
-  console.log("GET cont.logout");
+  console.log("GET cont/logout");
   res.send("logout sahifadasiz");
 };
 
+////////////////////////////////////////////////
 memberController.createToken = (result) => {
   try {
     const upload_data = {
@@ -65,6 +66,22 @@ memberController.createToken = (result) => {
 
     assert.ok(token, Definer.auth_err2);
     return token;
+  } catch (err) {
+    throw err;
+  }
+};
+
+memberController.checkMyAuthentication = (req, res) => {
+  try {
+    console.log("GET cont/checkMyAuthentication");
+    let token = req.cookies["access_token"];
+    console.log("token====", token);
+
+    //token ichidagi yashiringan objectni qo'lga kiritish logikasi
+    const member = token ? jwt.verify(token, process.env.SECRET_TOKEN) : null;
+    assert.ok(member, Definer.auth_err2);
+
+    res.json({ state: "succeed", data: member });
   } catch (err) {
     throw err;
   }
--- router.js
+++ router.js
@@ -12,6 +12,8 @@ const memberController = require('./controllers/memberController')
 router.post("/signup", memberController.signup);
 router.post("/login", memberController.login);
 router.get("/logout", memberController.logout);
+router.get("/check-me", memberController.checkMyAuthentication);
+
 
 
 // boshqa routerlar
